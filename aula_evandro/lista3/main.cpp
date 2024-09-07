bcddefbd56808cb2e9321200a4df02510e608a68
luisgustavo <luisgustavosr333@gmail.com>
2024-09-06 23:52:35 -0300

    ex 40%

--- aula_evandro/lista3/main.cpp
+++ aula_evandro/lista3/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
 
 struct ContaDeCliente {
     std::string nome;
@@ -10,26 +12,28 @@ struct ContaDeCliente {
 };
 
 void mostra_menu_de_opcoes();
-int recebe_opcao_valida();
+unsigned int recebe_opcao_valida();
 void adiciona_nova_conta(ContaDeCliente&);
 std::string recebe_nome_valido();
 std::string recebe_cpf_valido();
-void recebe_endereco_valido(std::string [], int);
+void recebe_endereco_valido(std::string [], int = 3);
+std::string recebe_telefone_valido();
+double recebe_saldo_valido();
 
 const int TAM = 10;
 #ifdef TESTE
 //bloco para testes
 int main(){
-    mostra_menu_de_opcoes();
+    // mostra_menu_de_opcoes();
     int opcao = recebe_opcao_valida();
-
-    return 0;
+    std::string telefone = recebe_telefone_valido();
+    std::cout << "\n" << telefone << "\n";
 }
 #else
 //bloco principal
 int main(){
     ContaDeCliente clientes[TAM]={""};
-    int contaCliente = 0;
+    int numContas = 0;
 
     mostra_menu_de_opcoes();
     int opcao = recebe_opcao_valida();
@@ -37,11 +41,12 @@ int main(){
     switch (opcao)
     {
     case 1:
-        //adiciona_nova_conta(clientes[contaCliente])
-        contaCliente++;
+        adiciona_nova_conta(clientes[numContas]);
+        numContas++;
         break;
     case 2:
         //remove_conta_cliente
+        numContas--;
         break;
     case 3:
         //lista_contas_cadastradas
@@ -69,14 +74,14 @@ std::cout << "(4) - Imprimir conta específica.\n";
 std::cout << "(5) - Atualizar conta de cliente.\n";
 };
 
-int recebe_opcao_valida(){
-    int opcao;
+unsigned int recebe_opcao_valida(){
+    unsigned int opcao;
     do
     {
         std::cout << "Opcao:";
         std::cin >> opcao;
     } while (opcao < 1 or opcao > 5);
-    
+    std::cin.ignore();
     return opcao;
 }
 
@@ -86,6 +91,7 @@ void adiciona_nova_conta(ContaDeCliente& cliente){
     cliente.cpf = recebe_cpf_valido();
     recebe_endereco_valido(cliente.endereco);
     cliente.telefone = recebe_telefone_valido();
+    cliente.saldo = recebe_saldo_valido();
     
 };
 
@@ -94,83 +100,116 @@ std::string recebe_nome_valido(){
     do
     {
         std::cout << "Nome: ";
-        std::cin.ignore();
         std::getline(std::cin,nome);
-    } while (nome.empty());
+    } while (nome.empty() || nome == "\n" || nome == " ");
     return nome;
 };
 
 std::string recebe_cpf_valido(){
     std::string cpf;
+    std::string cpf_limpo = "";
     while (true)
     {
+        cpf_limpo = "";
         std::cout << "cpf: ";
-        std::cin.ignore();
         std::getline(std::cin,cpf);
         for (int i = 0; i < cpf.length(); i++){
 
-            if (!isdigit(cpf[i])){
-                cpf.erase(i);
-                --i;
+            if (isdigit(cpf[i])){
+                cpf_limpo += cpf[i];
             }
         }
         
-        if(cpf.size() != 11){
+        if(cpf_limpo.size() != 11){
             std::cout << "cpf inválido!\n";
         }else{
             break;
         }
     }
     
-    return cpf;
+    return cpf_limpo;
 };
 
-void recebe_endereco_valido(std::string endereco[],int tam = 3){
+void recebe_endereco_valido(std::string endereco[],int tam){
     do
     {
     std::cout << "Cidade: ";
-    std::cin.ignore();
+   
     std::getline(std::cin,endereco[0]);
     } while (endereco[0] == "");
 
+    std::string uf_limpo = "";
     do
     {
+        uf_limpo = "";
         std::cout << "UF: ";
-        std::cin.ignore();
         std::getline(std::cin,endereco[1]);
-        for (int i = 0; i < endereco[1].size(); i++){
+        for(char c : endereco[1]){
 
-            if(!isalpha(endereco[1].at(i))){
-                endereco[1].at(i) = toupper(endereco[1].at(i));
-                endereco[1].erase(i);
-                --i;
+            if (isalpha(c)){
+                uf_limpo += toupper(c);
             }
         }
 
-        if(endereco[1].size() != 2){
+        if(uf_limpo.size() != 2){
             std::cout << "UF inválido! formato deve ser: \"SP\"\"SC\" \n";
         }else{
+            endereco[1] = uf_limpo;
             break;
         }
         
     } while (true);
 
+    std::string cep_limpo;
     do
     {
+        cep_limpo="";
         std::cout << "CEP: ";
-        std::cin.ignore();
         std::getline(std::cin, endereco[2]);
-        for (int i = 0; i < endereco[2].size(); i++){
-
-            if(!isdigit(endereco[2].at(i))){
-                endereco[2].erase(i);
-                --i;
+        for(char c : endereco[2]){
+            if(isdigit(c)){
+                cep_limpo += c;
             }
         }
-        if(endereco[2].size() != 8){
+        if(cep_limpo.size() != 8){
             std::cout << "Endereço inválido!\n";
         }else{
+            endereco[2] = cep_limpo;
             break;
         }
     } while (true);
 }
+
+std::string recebe_telefone_valido(){
+    std::string telefone;
+    std::string telefone_limpo;
+    do{
+        telefone_limpo = "";
+        std::cout << "formato de telefone: (DDD) 9XXXX-YYYY\n";
+        std::cout << "telefone: ";
+       
+        std::getline(std::cin,telefone);
+        for(char c : telefone){
+            if(isdigit(c)){
+                telefone_limpo += c;
+            }
+        }
+        if(telefone_limpo.size() > 12 || telefone_limpo.size() < 10){
+            std::cout << "Formato telefônico inválido!\n";
+        }else{
+            break;
+        }
+    }while(true);
+    return telefone_limpo;
+};
+
+double recebe_saldo_valido(){
+    double saldo;
+    do
+    {
+        std::cout << "Saldo na conta: ";
+        std::cin >> saldo;
+    } while (saldo < 0);
+    
+    return saldo;
+};
