4718be836a79c00ad5fb630a798c99ee7a22abc4
luisgustavo <luisgustavosr333@gmail.com>
2024-10-02 08:14:34 -0300

    push

--- aula_evandro/lista6_classe/classe_estoque/main.cpp
+++ aula_evandro/lista6_classe/classe_estoque/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     Estoque e(12314,100,3.50);
-    std::cout << e.obter_codigo_item() << "\n" << e.obter_custo() << "\n" << e.obter_quantidade() << "\n" << e.obter_custo_total() << std::endl;
+    std::cout << e.obter_codigo_item() << "\n" << e.obter_custo() << "\n" << e.obter_quantidade() << "\n" << e.obter_custo_total() << "\n\n"<< std::endl;
 
     Estoque f;
     f.definir_custo(7.50);
